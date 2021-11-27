3eee0370532dd8e4a75dfa366c9944913af86168
bhkim <bhkim0620@naver.com>
2021-11-27 18:47:44 +0900

    make result path

--- app/main.py
+++ app/main.py
@@ -14,7 +14,11 @@ def detail():
       detail['StudentNumber'] = request.form.get('StudentNumber')
       detail['Gender'] = request.form.get('Gender')
       detail['Major'] = request.form.get('Major')
-      return render_template("detail.html",detail = detail)
+      return render_template("detail.html", detail=detail)
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+   pass
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", debug=True, port=80)
