87ad3103e2888cdf9f531f180fd4c2fe25520447
LeeJY98 <wldudwldhd@naver.com>
2021-11-27 16:53:32 +0900

    teamproject1

--- app/main.py
+++ app/main.py
@@ -4,18 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def student():
-   return render_template('student_info.html')
+   return render_template('main.html')
 
-@app.route('/result', methods = ['POST', 'GET'])
-def result():
+@app.route('/detail', methods = ['POST', 'GET'])
+def detail():
    if request.method == 'POST':
-      result = dict()
-      result['Name'] = request.form.get('Name')
-      result['StudentNumber'] = request.form.get('StudentNumber')
-      result['Gender'] = request.form.get('Gender')
-      result['Major'] = request.form.get('Major')
-      result['languages'] = ', '.join(request.form.getlist('languages'))
-      return render_template("result.html",result = result)
+      detail = dict()
+      detail['Name'] = request.form.get('Name')
+      detail['StudentNumber'] = request.form.get('StudentNumber')
+      detail['Gender'] = request.form.get('Gender')
+      detail['Major'] = request.form.get('Major')
+      return render_template("detail.html",detail = detail)
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", debug=True, port=80)
